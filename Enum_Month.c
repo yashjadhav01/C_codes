9c7dfb50b6d8c2d68e2729ce8f468128a545852f
Yash Jadhav <146317569+yashjadhav01@users.noreply.github.com>
2023-10-06 10:34:55 +0530

    Enum

---
+++ Enum_Month.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+enum month{JAN=1,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC};
+
+
+int main(){
+printf("JAN %d\n",JAN);
+printf("FEB %d\n",FEB);
+printf("MAR %d\n",MAR);
+printf("APR %d\n",APR);
+printf("MAY %d\n",MAY);
+printf("JUN %d\n",JUN);
+printf("JUL %d\n",JUL);
+printf("AUG %d\n",AUG);
+printf("SEP %d\n",SEP);
+printf("OCT %d\n",OCT);
+printf("NOV %d\n",NOV);
+printf("DEC %d\n",DEC);
+
+    return 0;
+}
--- enum.c
+++ enum.c
@@ -1,21 +1,30 @@
 #include<stdio.h>
 enum Level {
-LOW = 1,
+LOW=1,
 MEDIUM,
-HIGH
+HIGH,
 };
 int main() {
- enum Level myVar =MEDIUM;
- switch (myVar) {
- case 1:
- printf("Low Level");
- break;
- case 2:
- printf("Medium level");
- break;
- case 3:
- printf("High level");
- break;
+
+enum Level Myvar=1;
+// printf("Enter the number");
+// scanf("%d",&Myvar);
+switch (Myvar)
+{
+case 1:
+    printf("low level");
+    break;
+case 2:
+    printf("medium level");
+    break;
+case 3:
+    printf("High level");
+    break;    
+
+default:
+printf("Please enter the correct level");
+    break;
 }
- return 0;
+return 0;
+
 }
